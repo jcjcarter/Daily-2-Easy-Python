d5b34391970e1a2b467f89fc51bcd52935d7f621
jcjcarter <allstatetexan@hotmail.com>
2015-10-21 14:01:10 -0500

    Initial.

---
+++ Daily 2 Easy Python/Daily_2_Easy_Python.py
@@ -0,0 +1,22 @@
+﻿import unittest
+import imAlgo as jkl
+
+""" Generate unit tests first. """
+
+class TestCalMethods(unittest.TestCase):
+
+    def test_getForce(self):
+        self.assertEqual(0, jkl.getForce())
+
+    def test_getAcceleration(self):
+        self.assertEqual(0, jkl.getAcceleration())
+
+    """ Creates """
+    def test_computeForce(self):
+        jkl.setMass()
+        for i in range(100):
+            self.assertEqual(i*0, jkl.computeForce())
+
+if __name__ == "__main__":
+    unittest.main()
+    
---
+++ Daily 2 Easy Python/imAlgo.py
@@ -0,0 +1,17 @@
+force = 0
+mass = 0
+acceleration = 0
+
+def getForce():
+    return force
+
+def getAcceleration():
+    return acceleration
+# Sets the mass
+def setMass():
+    mass = 90
+   
+
+
+def computeForce():
+    return mass * acceleration
